d69d4558f65e05bbfc40b7700ae48ee3354e8e52
riandomt <mittou.dorian@gmail.com>
2025-10-17 14:12:12 +0200

    Ajout du header/footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,16 @@
-function openDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-content');
-
-    if (!isVisible(dropdownContent))
-        Object.assign(dropdownContent.style, {
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            top: "75px",
-            backgroundColor: "#FFFFFF",
-            padding: "25px",
-            boxShadow: "0 4px 4px rgba(0, 0, 0, 0.25)"
-        }); else {
-        dropdownContent.style.cssText = "display:none;";
-    }
+function isVisible(elem){
+  return elem && getComputedStyle(elem).display!=="none";
 }
 
-function openNavbar() {
-    const navbar = document.querySelector('nav');
-    if (!isVisible(navbar)) {
-        Object.assign(navbar.style, {
-            display: "flex",
-            flexDirection: "column",
-            position:"absolute",
-            top:"100px"
-        });
-    } else {
-        navbar.style.cssText = "display:none;";
-    }
+function openNavbar(){
+  const navbar=document.querySelector('nav');
+  if(!navbar)return;
+  navbar.classList.toggle('open');
 }
 
-function isVisible(element) {
-    return element.style.display === "none" ? false : true;
+function openDropdown(event){
+  event.preventDefault();
+  const dropdown=event.currentTarget.closest('.dropdown');
+  if(!dropdown)return;
+  dropdown.classList.toggle('open');
 }
